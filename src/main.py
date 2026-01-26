9e6904f488025a0e3aa53450723c935b71c0f37e
Anil Kumar <126413189+aniljiA1@users.noreply.github.com>
2026-01-26 12:06:19 +0530

    Updated

--- src/main.py
+++ src/main.py
@@ -1,24 +1,20 @@
-from .gmail_service import authenticate_gmail, fetch_unread_emails, mark_as_read
-from .sheets_service import (
-    authenticate_sheets,
-    append_to_sheet,
-    load_state,
-    save_state
-)
-from .email_parser import extract_email_data
+from src.gmail_service import authenticate_gmail, fetch_unread_emails, mark_as_read
+from src.sheets_service import authenticate_sheets, append_to_sheet, load_state, save_state
+from src.email_parser import extract_email_data
 from config import STATE_FILE
+
 
 def main():
     gmail_service = authenticate_gmail()
     sheets_service = authenticate_sheets()
+
     state = load_state(STATE_FILE)
+    unread_messages = fetch_unread_emails(gmail_service)
 
-    messages = fetch_unread_emails(gmail_service)
+    for message in unread_messages:
+        msg_id = message.get("id")
 
-    for msg in messages:
-        msg_id = msg["id"]
-
-        if msg_id in state["processed_ids"]:
+        if not msg_id or msg_id in state["processed_ids"]:
             continue
 
         email = extract_email_data(gmail_service, msg_id)
@@ -37,6 +33,7 @@ def main():
         state["processed_ids"].append(msg_id)
 
     save_state(STATE_FILE, state)
+
 
 if __name__ == "__main__":
     main()
